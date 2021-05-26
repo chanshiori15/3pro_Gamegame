b22a544d3faacb2c53c7bda5507795ba93976f20
chanshiori <pinik.tegos.bubbles.s@gmail.com>
2021-05-26 13:51:49 +0900

    バージョン変更

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    //public SelectScript selectScript;
+    public Text timerLabel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerLabel.text = SelectScript.time.ToString("f1");
+    }
+
+    public void OnClick()
+    {
+        SelectScript.time -= Time.deltaTime;
+    }
+}
